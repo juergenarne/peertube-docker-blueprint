206fec21f5f4da95ea11bfdae6f500999a12a2d4
juergenarne <juergenarne@users.noreply.github.com>
2025-06-23 18:55:27 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,8 @@
 cd src
 npm run build
 mkdir logs
+
+export NODE_ENV=production
+nohup npm run start -- --port 9000 > logs/peertube-server.log 2>&1 &
+
 #nohup NODE_ENV=production npm run start -- --port 9000 > logs/peertube-server.log 2>&1 &
