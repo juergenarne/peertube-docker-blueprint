9436f08d3ab829010f186986fec3a77ea119ac46
juergenarne <juergenarne@users.noreply.github.com>
2025-06-23 17:33:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,8 +19,15 @@ npm i -g yarn
 npm i
 
 yarn install --pure-lockfile
-npm run build
 
-nohup npm run start -- --port 9000 > /var/www/peertube/peertube-server.log 2>&1 &
+cd config
+cp production.yaml.example production.yaml
+cd -
+# npm run build
 
-echo "Peertube is up and running"
+# nohup npm run start -- --port 9000 > /var/www/peertube/peertube-server.log 2>&1 &
+
+echo "Peertube ready for configuaration"  
+echo "After adjusting your `production.yaml` according to your needs: "
+echo ">npm run build"
+echo "nohup npm run start -- --port 9000 > logs/peertube-server.log 2>&1 &"
